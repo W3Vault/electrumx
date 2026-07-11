54b1d44618944c9a08b331b72c2fb82ee8c9010c
5ha2s6 <tech@5ha2s6.com>
2026-07-11 12:51:36 +0100

    Register Wiiicoin with ElectrumX

--- src/electrumx/__init__.py
+++ src/electrumx/__init__.py
@@ -4,8 +4,12 @@ PYTHON_MIN_VERSION = (3, 10)  # noqa: E402
 from electrumx.server.controller import Controller
 from electrumx.server.env import Env
 
+# Import custom coin definitions after the core server modules have loaded.
+# Coin.lookup_coin_class() discovers subclasses dynamically.
+from electrumx.lib.coins_wiiicoin import Wiiicoin as _Wiiicoin  # noqa: F401,E402
 
-BRANDING = "spesmilo"
+
+BRANDING = "W3Vault"
 __version__ = "2.0.0"
 version = f'ElectrumX {__version__}'
 version_short = __version__
